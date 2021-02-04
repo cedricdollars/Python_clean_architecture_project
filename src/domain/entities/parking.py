1d6aeb58ed85d59f530d0b95cc8786c670e509eb
ngoune_cedric <gabyngoune@yahoo.fr>
2021-02-04 23:18:23 +0100

    usescases and models

--- src/domain/entities/parking.py
+++ src/domain/entities/parking.py
@@ -1,1 +1,12 @@
 # coding:utf-8
+
+class Parking(object):
+    def __init__(self, total_places, free_places, used_places):
+       self.total_places = 13
+       self.used_places = 0
+       self.free_places = total_places - used_places
+       
+        
+        
+
+    
--- src/domain/entities/ticket.py
+++ src/domain/entities/ticket.py
@@ -1,2 +1,7 @@
 #coding:utf-8
-## TODO
+
+class Ticket(object):
+    def __init__(self, number_of_place, date_arrived, car_info):
+        self.number_of_place = number_of_place
+        self.date_arrived = date_arrived
+        self.car_info = car_info
